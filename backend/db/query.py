b97965e558a4c6e988c955dc6443301c333178d3
balaji227 <balaji.balaji235@gmail.com>
2024-06-13 19:03:23 +0530

    reference from notebook

--- backend/db/query.py
+++ backend/db/query.py
@@ -1,4 +1,16 @@
 from models.chatbot import Query
-from pymongo import MongoClient
+import pymongo
+import os
+import urllib
+
+
+COSMOS_MONGO_USER = os.getenv("COSMOS_MONGO_USER")
+COSMOS_MONGO_PWD = os.getenv("COSMOS_MONGO_PWD")
+COSMOS_MONGO_SERVER = os.getenv("COSMOS_MONGO_SERVER")
+
+mongo_conn = "mongodb+srv://"+urllib.parse.quote(COSMOS_MONGO_USER)+":"+urllib.parse.quote(COSMOS_MONGO_PWD)+"@"+COSMOS_MONGO_SERVER
+mongo_client = pymongo.MongoClient(mongo_conn)
+db = mongo_client['chatbot']
 
 def create(query: Query):
+    db['queries'].insert_one(query) 
--- backend/routers/chatbot.py
+++ backend/routers/chatbot.py
@@ -1,9 +1,26 @@
 from fastapi import APIRouter
 from models.chatbot import Query
 import db.query
+from openai import AzureOpenAI
+import time
+import os
 
 router = APIRouter()
 
 @router.post("/query", tags=["query"])
 async def query_create(query: Query):
     db.query.create(Query)
+
+
+
+openai_client = AzureOpenAI(api_key=os.getenv("AZURE_OPENAI_KEY"), azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT"), api_version="2024-02-01",)
+
+def generate_embeddings(text):
+    '''
+    Generate embeddings from string of text.
+    This will be used to vectorize data and user input for interactions with Azure OpenAI.
+    '''
+    response = openai_client.embeddings.create(input=text, model="text-embedding-ada-002")
+    embeddings =response.model_dump()
+    time.sleep(0.5) 
+    return embeddings['data'][0]['embedding']
