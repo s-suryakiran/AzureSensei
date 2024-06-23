f534dca4a7c45325253a13a7b4186fa7dac3d8c3
balaji227 <balaji.balaji235@gmail.com>
2024-06-23 21:35:53 +0530

    add sidebar showing history

--- backend/app.py
+++ backend/app.py
@@ -1,29 +1,16 @@
 import streamlit as st
 import chatbot as chatbot
 
-st.title("Welcome To Azure Sensei")
+st.title("Welcome To Azure Sensei")\
 
-# # Initialize chat history
-# if "messages" not in st.session_state:
-#     st.session_state.messages = []
+# Sidebar for chat history
+st.sidebar.title("Chat History")
 
-# # Display chat messages from history on app rerun
-# for message in st.session_state.messages:
-#     with st.chat_message(message["role"]):
-#         st.markdown(message["content"])
+if "messages" not in st.session_state:
+    st.session_state.messages = []
 
-
-# if prompt := st.chat_input("Ask your question"):
-#     st.session_state.messages.append({"role": "user", "content": prompt})
-#     with st.chat_message("user"):
-#         st.markdown(prompt)
-
-#     with st.chat_message("assistant"):
-#         for m in st.session_state.messages:
-#             chatbot.query_create(
-#                 qry=models.chatbot.Query(role=m["role"], query=m["content"])
-#             )
-
+for i, message in enumerate(st.session_state.messages):
+    st.sidebar.write(f"{i+1}. {message['role'].capitalize()}: {message['content']}")
 
 if "messages" not in st.session_state:
     st.session_state.messages = []
